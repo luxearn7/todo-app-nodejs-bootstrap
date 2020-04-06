f1cf0c3d180a424d4d0924410dd84f3bb8c1f0d0
Luxearn7 <luxearn7@gmail.com>
2020-04-06 21:31:02 +0700

    Finish app

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -3,26 +3,21 @@ const router = express.Router()
 const taskModel = require('../models/taskModel')
 
 //Get all tasks
-router.get('/', async function(req, res, next) {
-    // let search = {}
-    // if(req.query.task != null && req.query.task !='') {
-    //     search.task = new RegExp(req.query.task, 'i')
-    //}
+router.get('/', async function(req, res, next) {    
     try {
         const tasks = await taskModel.find({})
-        res.render('taskView', {tasks: tasks})
-        //res.render('taskView', {tasks: tasks})
+        res.render('taskView', {tasks: tasks})        
     } catch {
         res.redirect('/')
     }
 })
 
+//Get a task
 router.get('/:id', async function(req, res, next) {
     try {
         const task = await taskModel.findById(req.params.id)
         const tasks = await taskModel.find({})
-        res.render('taskView', {tasks: tasks})
-        //res.render('taskView', {tasks: tasks})
+        res.render('taskView', {tasks: tasks})       
     } catch {
         res.render('taskView', {
             errorMessage: 'Error creating Task'
@@ -30,7 +25,7 @@ router.get('/:id', async function(req, res, next) {
     }
 })
 
-//Edit a task
+//Get edit task
 router.get('/edit/:id', async function(req, res, next) {
     try {
         const task = await taskModel.findById(req.params.id)
@@ -50,7 +45,7 @@ router.post('/', async function(req, res, next) {
         res.redirect('/task')
     } catch {
         const tasks = await taskModel.find({})
-        res.render('taskView', {tasks: tasks, errorMessage: 'You must write a task'})
+        res.render('taskView', {tasks: tasks, errorMessage: 'Error: You must write a task'})
     }
 })
 
@@ -61,7 +56,6 @@ router.put('/edit/:id', async function(req, res, next) {
         task = await taskModel.findById(req.params.id)
         task.task = req.body.task
         await task.save()
-        // 
         res.redirect('/task')
     } catch {
         if (task == null) {
@@ -69,11 +63,12 @@ router.put('/edit/:id', async function(req, res, next) {
           } else {
             res.render('editView', {
               task: task,
-              errorMessage: 'Error updating Task'
+              errorMessage: 'Error: You must write a task'
             })
           }
     }
 })
+
 //Delete task
 router.delete('/:id', async function(req, res, nect) {
     let task
--- server.js
+++ server.js
@@ -1,41 +1,40 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const Layouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/indexRouter')
 const taskRouter = require('./routes/taskRouter')
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.set(express.static('public'));
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
+
+app.use(express.static('public'))
 app.use(Layouts)
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 
 mongoose.connect(process.env.DB_URL, { 
     useUnifiedTopology: true,
     useNewUrlParser: true 
- });
-const db = mongoose.connection;
+ })
+const db = mongoose.connection
 db.on('error', function(error) {
-    console.error(error);    
-});
+    console.error(error)    
+})
 db.once('open', function() {
-    console.log('Connect success!');    
-});
+    console.log('Connect success!')    
+})
 
-//-----------------------------------------
+//---------------------------------
 app.use('/', indexRouter)
-app.use('/task', taskRouter);
+app.use('/task', taskRouter)
 
 app.listen(process.env.PORT, function() {
-    console.log('Server running...');    
-});
+    console.log('Server running...')    
+})

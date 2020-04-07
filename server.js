e4e9bb4f30623025819cf7513d8f047fdf1cf268
Luxearn7 <luxearn7@gmail.com>
2020-04-07 15:01:19 +0700

    Finish app

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ db.once('open', function() {
 app.use('/', indexRouter)
 app.use('/task', taskRouter)
 
-app.listen(process.env.PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Server running...')    
 })
